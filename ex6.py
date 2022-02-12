ed2bb01764cdfc1ec5195678cc56adc9282ea622
Alexandr Kokorev <aleksandr.kokorev@instamart.ru>
2022-02-12 16:40:05 +0300

    added comment for ex6

--- ex6.py
+++ ex6.py
@@ -3,3 +3,6 @@ import requests
 response = requests.get('https://playground.learnqa.ru/api/long_redirect')
 print('Кол-во редиректов: ' + str(len(response.history)))
 print('Конечный url: ' + response.url)
+
+#Кол-во редиректов: 3
+#Конечный url: https://www.learnqa.ru/
